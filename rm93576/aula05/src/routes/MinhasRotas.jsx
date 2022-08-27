b9000580c98faea685d199f72672e44b6b8d7454
abner-ferreira <rm93576@fiap.com.br>
2022-08-26 22:58:19 -0300

    Iniciado o EventDriven

---
+++ rm93576/aula05/src/routes/MinhasRotas.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import Home from "../components/home/Home"
+import NovoValor from "../components/novoValor/NovoValor"
+
+export default function MinhasRotas() {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/nv" element={<NovoValor/>}/>
+            </Routes>
+        </>
+    )
+}
