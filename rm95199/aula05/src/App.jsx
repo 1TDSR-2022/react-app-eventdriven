20b5b6f1a2a20f9422fa4e0b6856bcb2f2c12498
emersonnfs <rm95199@fiap.com.br>
2022-08-26 22:29:03 -0300

    app criado

---
+++ rm95199/aula05/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MinhasRotas from './routes/MinhasRotas'
+import Menu from './components/menu/Menu'
+
+export default function App() {
+  return (
+    <div>
+      <Menu />
+      <h2>APP</h2>
+        <MinhasRotas />
+        
+    </div>
+  )
+} 
---
+++ rm95199/aula05/src/components/novoValor/NovoValor.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function NovoValor() {
+    return(
+        <div>
+            <h2>Vamos escrever uma frase</h2>
+            <div>
+                <label>Motivação : </label>
+                <input type="text" />
+                <p>Mtivação do dia <span></span></p>
+            </div>
+        </div>       
+    )
+}
